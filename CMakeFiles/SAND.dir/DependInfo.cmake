cc03bc7b52505866c84a2e20a1e6ac693f5d517b
juleoc02 <juleoc@colostate.edu>
2020-09-29 08:30:02 -0600

    trying to get gitignore back

---
+++ CMakeFiles/SAND.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/justin/Thesis/topo code/SAND/source/SAND.cc" "/home/justin/Thesis/topo code/SAND/CMakeFiles/SAND.dir/source/SAND.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_NO_AUTO_PTR"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/justin/deal.ii-candi/deal.II-v9.2.0/include"
+  "/home/justin/deal.ii-candi/deal.II-v9.2.0/include/deal.II/bundled"
+  "/home/justin/deal.ii-candi/parmetis-4.0.3/include"
+  "/home/justin/deal.ii-candi/trilinos-release-12-18-1/include"
+  "/usr/include/suitesparse"
+  "/home/justin/deal.ii-candi/petsc-3.13.1/include"
+  "/home/justin/deal.ii-candi/hdf5-1.10.5/include"
+  "/home/justin/deal.ii-candi/oce-OCE-0.18.2/include/oce"
+  "/home/justin/deal.ii-candi/p4est-2.2/FAST/include"
+  "/home/justin/deal.ii-candi/slepc-3.13.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/SAND.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SAND.dir/source/SAND.cc.o"
+  "SAND"
+  "SAND.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SAND.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
