ada76760bdee29f74012431126e50a7051fdffbe
juleoc02 <juleoc@colostate.edu>
2021-02-06 13:45:25 -0700

    Fixed bad index, changed size to 6 for running on big school computer

--- source/SAND.cc
+++ source/SAND.cc
@@ -260,7 +260,7 @@ namespace SAND {
             GridGenerator::merge_triangulations(triangulation_temp,
                                                 triangulation, triangulation);
         }
-        triangulation.refine_global(5);
+        triangulation.refine_global(6);
 
         /*Set BCIDs   */
         for (const auto &cell : triangulation.active_cell_iterators()) {
