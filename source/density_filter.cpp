583abaa11574bd3a0bc4cf124d50d02a3af4516f
Justin O'Connor <juleoc@colostate.edu>
2021-05-25 14:34:08 -0600

    Added density filter object

---
+++ source/density_filter.cpp
@@ -0,0 +1,63 @@
+//
+// Created by justin on 5/13/21.
+//
+#include "../include/density_filter.h"
+
+#ifndef SAND_KKTSYSTEM_H
+#define SAND_KKTSYSTEM_H
+
+#include <deal.II/base/quadrature_lib.h>
+#include <deal.II/base/function.h>
+#include <deal.II/base/tensor.h>
+#include <deal.II/base/timer.h>
+
+#include <deal.II/lac/block_vector.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/block_sparse_matrix.h>
+#include <deal.II/lac/solver_cg.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/lac/linear_operator.h>
+#include <deal.II/lac/packaged_operation.h>
+#include <deal.II/lac/sparse_direct.h>
+#include <deal.II/lac/affine_constraints.h>
+
+#include <deal.II/grid/tria.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_refinement.h>
+
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/dofs/dof_renumbering.h>
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_values.h>
+#include <deal.II/fe/fe_dgq.h>
+#include <deal.II/fe/fe_system.h>
+#include <deal.II/fe/fe_q.h>
+
+#include <deal.II/numerics/vector_tools.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/error_estimator.h>
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+namespace SAND {
+    using namespace dealii;
+    template<int dim>
+    DensityFilter<dim>::DensityFilter(triangulation)
+            :
+            filter_r(.251)
+    {
+    }
+
+    template<int dim>
+    void
+    DensityFilter<dim>::initialize(Triangulation &triangulation)
+    {
+        const unsigned int n_cells = triangulation.cells
+
+    }
+
+}//SAND namespace
